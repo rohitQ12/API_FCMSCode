1446f832fef879d1cfa8ce02e7585b4764973092
Thushara65 <thusharathanu88@gmail.com>
2022-04-30 15:45:04 +0530

    test now

--- GlobalApi/Controllers/MasterController/DiseasesDtlController.cs
+++ GlobalApi/Controllers/MasterController/DiseasesDtlController.cs
@@ -31,21 +31,21 @@ namespace GlobalApi.Controllers.MasterController
         //        return BadRequest("Not successfull");
         //}
 
-        [HttpPut, Route("UpdateDiseasesDtl")]
-        public async Task<ActionResult<DiseasesDtl>> Put([FromBody] List<DiseasesDtl> lead, int App_id)
-        {
-            if (lead == null)
-            {
-                return BadRequest();
-            }
+        //[HttpPut, Route("UpdateDiseasesDtl")]
+        //public async Task<ActionResult<DiseasesDtl>> Put([FromBody] List<DiseasesDtl> lead, int App_id)
+        //{
+        //    if (lead == null)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            var change = await _repository.UpdateDiseasesDtltest(lead, App_id);
+        //    var change = await _repository.UpdateDiseasesDtltest(lead, App_id);
 
-            if (change == true)
-                return Ok();
-            else
-                return BadRequest("Not successfull");
-        }
+        //    if (change == true)
+        //        return Ok();
+        //    else
+        //        return BadRequest("Not successfull");
+        //}
 
         [HttpGet, Route("GetAllDiseasesDtl")]
         public async Task<ActionResult<IEnumerable<DiseasesDtl>>> GetAllDiseasesDtl()
