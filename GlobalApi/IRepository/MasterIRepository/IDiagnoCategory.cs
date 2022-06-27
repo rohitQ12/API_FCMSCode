c8dae4a9a1e635c413e0dd7c4a6e04da033e8ddf
Thushara65 <thusharathanu88@gmail.com>
2022-06-27 11:42:38 +0530

    test

--- GlobalApi/IRepository/MasterIRepository/IDiagnoCategory.cs
+++ GlobalApi/IRepository/MasterIRepository/IDiagnoCategory.cs
@@ -7,7 +7,7 @@ namespace GlobalApi.IRepository.MasterIRepository
         Task<DiagnoCategory> InsertDiagnoCategory(DiagnoCategory lead);
         Task<DiagnoCategory> UpdateDiagnoCategory(DiagnoCategory lead);
         Task<List<GetAllDiagnoCat>> GetAllDiagnoCategory();
-        Task<List<Diagno_DD>> GetDiagnoCategory_DD();
+        Task<List<Diagno_DD>> GetDiagnoCategory_DD(int Type_Id);
         //Task<DiagnoCategoryBy_Id> GetDiagnoCategoryById(int Id);
         Task<DiagnoCategory> DeleteDiagnoCategory(int Id);
 
