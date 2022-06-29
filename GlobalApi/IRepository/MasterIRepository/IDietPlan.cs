0566836a6787051f1c7944e6aa872963ab992bab
manumerkaje <manumerkaje@gmail.com>
2022-06-29 12:44:41 +0530

    new

--- GlobalApi/IRepository/MasterIRepository/IDietPlan.cs
+++ GlobalApi/IRepository/MasterIRepository/IDietPlan.cs
@@ -7,8 +7,8 @@ namespace GlobalApi.IRepository.MasterIRepository
         Task<DietPlan> InsertDietPlan(DietPlan lead);
         Task<DietPlan> UpdateDietPlan(DietPlan lead);
         Task<List<GetAllDietPlan>> GetAllDietPlan();
-        Task<GetById> GetDietPlanById(int SYM_Id);
-        Task<DietPlan> DeleteDietPlan(int SYM_Id);
+        Task<List<GetAllDietPlan>> GetDietPlanById(int Id);
+        Task<DietPlan> DeleteDietPlan(int Id);
 
     }
 }
