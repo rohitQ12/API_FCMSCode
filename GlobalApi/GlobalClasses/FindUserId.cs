2ee9bf9fe0f3b911901677f2f92ba5a3511e4cae
Thushara65 <thusharathanu88@gmail.com>
2022-07-13 15:07:59 +0530

    jhjh

--- GlobalApi/GlobalClasses/FindUserId.cs
+++ GlobalApi/GlobalClasses/FindUserId.cs
@@ -80,7 +80,7 @@ namespace GlobalApi.GlobalClasses
         public async Task<string> FindIdFromUserName(string userName)
         {
             AuthUser userDetails = await db.Users.SingleOrDefaultAsync(x => x.UserName == userName);
-            return userDetails.Id;
+             return userDetails.Id;
         }
         public async Task<int?> FindHospitalIdFromHospitalOfficeUsername(string userName)
         {
